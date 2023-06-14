7f611f5c8a095a4a482b74073a4d11cc18e18928
Kong19565 <khaogong48@gmail.com>
2023-06-14 15:37:45 +0700

    first commit

---
+++ web_01.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css" />
+
+<?php
+$name = "อภิสักก์ คงภักดี";
+$age = 25;
+$year = 2023;
+$byear = $year - $age;
+
+echo "<h1> Welcome </h1>
+        FROM $name</br>
+        born $byear</br></br>
+
+        <img src= 'images/Isagi.jpg'/>"
+
+?>
+
+<img src="images/pxfuel.jpg" class="img"/>
